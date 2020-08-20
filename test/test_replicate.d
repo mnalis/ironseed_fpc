6812809a57f04596e11132697406fcb3c4627ed3
Matija Nalis <mnalis-git@voyager.hr>
2020-08-20 15:22:45 +0200

    some tests

---
+++ test/test_replicate.d
@@ -0,0 +1,10 @@
+import std.array;
+import std.stdio;
+
+void main() {
+    auto arr = replicate(['s'], 5); // lazy version: http://dlang.org/phobos/std_range.html#repeat
+    // or
+    auto arr2 = ['s'].replicate(5);
+    writeln(arr);
+    writeln(arr2);
+}
